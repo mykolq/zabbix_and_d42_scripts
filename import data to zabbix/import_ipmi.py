76bd3feefcedacf0c874134de1ea3b06d2dc671b
mykolq <mykolaoq@gmail.com>
2019-10-30 17:55:17 +0300

    Create import_ipmi.py

---
+++ import data to zabbix/import_ipmi.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+#This script reads information from a csv file and fills in the inventory fields in zabbix hosts
+#There is one command line argument: path to csv file
+
+import sys,csv
+from pyzabbix import ZabbixAPI
+
+#Fields from csv file
+csv_hostname = ""
+csv_ipmi = ""
+
+#List of founded zabbix hosts
+zabbix_hostnames = []
+
+#macri nane to update
+macroname='{$IPMIIP}'
+
+#Get list of zabbix hosts
+def Get_Zabbix_Hostnames():
+    global zabbix
+    zabbix = ZabbixAPI('http://localhost/zabbix', user='', password='')
+    zabbix.session.verify = False
+    hosts=zabbix.host.get(output=['hostid','name'])
+    for host in hosts:
+        zabbix_hostnames.append(str.lower((host['name']).encode('ascii','ignore')))
+
+#Get the id of hosts depending on their name
+def Get_Host_ID(name):
+    hosts=zabbix.host.get(output=['name','hostid'],filter={"name": name})
+    id_host = (hosts[0]['hostid'])
+    return id_host
+
+#Insert values from csv into Zabbix Inventory fields
+def Update_ipmi():
+    Get_Zabbix_Hostnames()
+    with open(sys.argv[1], 'r') as myfile:
+     inp_file = csv.reader(myfile, delimiter=',')
+     for line in inp_file:
+        csv_hostname = line[0]
+        csv_ipmi = line[1]
+        if(str.lower(csv_hostname) in zabbix_hostnames):
+            hid=Get_Host_ID(csv_hostname)
+            macros=zabbix.usermacro.get(hostids=hid, output=['value'], filter={'macro':macroname})
+            if len(macros)==0:
+                zabbix.usermacro.create(hostid=hid,macro=macroname,value=csv_ipmi)
+            else:
+                zabbix.usermacro.update(hostmacroid=macros[0]['hostmacroid'],value=csv_ipmi)
+
+Update_ipmi()
